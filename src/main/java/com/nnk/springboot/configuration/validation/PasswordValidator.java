178a12b3712fd7eb7e0e2e59fb91308b2d8d0e32
Ludovic Allegaert <allegaertl@gmail.com>
2021-08-19 10:48:21 +0200

    modification regex PasswordValidator

--- src/main/java/com/nnk/springboot/configuration/validation/PasswordValidator.java
+++ src/main/java/com/nnk/springboot/configuration/validation/PasswordValidator.java
@@ -8,8 +8,8 @@ import java.util.regex.Pattern;
 public class PasswordValidator implements ConstraintValidator<ValidPassword, String> {
 
     // digit + lowercase char + uppercase char + punctuation + symbol
-    //private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
-    private static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&./])[A-Za-z0-9@$!%*#?&./]{8,}$";
+    private static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,}$";
+    //private static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*#?&./])[A-Za-z0-9@$!%*#?&./]{8,}$";
 
     private static final Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
 
